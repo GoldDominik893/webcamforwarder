da7896b9debd1c2d340490a91751f82f31d7fe41
Dominik Wajda <admin@epicsite.xyz>
2022-10-11 17:28:35 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-from flask import Flask, Response
+from flask import Flask, Response, send_file
 import cv2
 import socket
 
 h_name = socket.gethostname()
 IP_address = socket.gethostbyname(h_name)
-webcontent = open('index.html').read()
-css = open('main.css').read()
+	
 print(r"""
+
   ______             __        __  _______                           __            __  __         ______    ______    ______   __                               
  /      \           |  \      |  \|       \                         |  \          |  \|  \       /      \  /      \  /      \ |  \                              
 |  $$$$$$\  ______  | $$  ____| $$| $$$$$$$\  ______   ______ ____   \$$ _______   \$$| $$   __ |  $$$$$$\|  $$$$$$\|  $$$$$$\| $$_______                       
@@ -17,7 +17,8 @@ print(r"""
  \$$    $$ \$$    $$| $$ \$$    $$| $$    $$ \$$    $$| $$ | $$ | $$| $$| $$  | $$| $$| $$  \$$\ \$$    $$ \$$    $$ \$$    $$  |       $$                      
   \$$$$$$   \$$$$$$  \$$  \$$$$$$$ \$$$$$$$   \$$$$$$  \$$  \$$  \$$ \$$ \$$   \$$ \$$ \$$   \$$  \$$$$$$   \$$$$$$   \$$$$$$    \$$$$$$$                       
                                                                                                                                                                 
-                                                                                                                                                                                                                                                                                                                                
+                                                                                                                                                                
+                                                                                                                                                                
                          __                                          ______                                                              __                     
                         |  \                                        /      \                                                            |  \                    
  __   __   __   ______  | $$____    _______  ______   ______ ____  |  $$$$$$\ ______    ______   __   __   __   ______    ______    ____| $$  ______    ______  
@@ -28,8 +29,9 @@ print(r"""
  \$$   $$   $$ \$$     \| $$    $$ \$$     \\$$    $$| $$ | $$ | $$| $$      \$$    $$| $$       \$$   $$   $$ \$$    $$| $$       \$$    $$ \$$     \| $$      
   \$$$$$\$$$$   \$$$$$$$ \$$$$$$$   \$$$$$$$ \$$$$$$$ \$$  \$$  \$$ \$$       \$$$$$$  \$$        \$$$$$\$$$$   \$$$$$$$ \$$        \$$$$$$$  \$$$$$$$ \$$      
                                                                                                                                                                 
-                                                                                                                                                                
+
 """)
+
 usercam = int(input('Choose camera output (0-5): '))
 if usercam == 0:
     camera = cv2.VideoCapture(0)
@@ -46,9 +48,12 @@ elif usercam == 5:
 
 wm = ("On another device please go to your computer's ip address (http://" + IP_address + ":5000)")
 print(wm)
+
 app = Flask(__name__)
+
 def gen_frames(): 
     while True:
+
         success, frame = camera.read()
         if not success:
             break
@@ -58,21 +63,20 @@ def gen_frames():
             yield (b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
+
 @app.route('/video_feed')
 def video_feed():
     """live image"""
     return Response(gen_frames(),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
-@app.route('/')
+@app.route("/")
 def index():
-    """webcam live view"""
-    return webcontent
+    return send_file("index.html")
 
-@app.route('/main.css')
-def cssdoc():
-    """css"""
-    return css
+@app.route("/main.css")
+def css():
+    return send_file("main.css")
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port = 5000, threaded = True)
